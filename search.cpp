03a3db5f2a798adc316d74c261311e34c4cbc8d8
juiceman007 <juiceman007@users.noreply.github.com>
2018-03-14 11:28:42 -0400

    algos

--- search.cpp
+++ search.cpp
@@ -12,7 +12,19 @@ using namespace std;
 \param [in] data The data set that will be searched
 \returns location of key if found or -1 if not found
 */
-int linearSearch(auto data, auto key);//prototype
+int linearSearch(auto data, auto key)
+{
+  for(auto i = 0u; i < data.size(); i++)
+   {   
+        
+     if (data[i] == key)//key found
+      return i;
+  }
+   return -1;//not found
+}
+	
+	
+
 
 
 int main()
